9cc12b98c1ac1c55f4161936f9931485af577026
JanSharp <steinmeister36@gmail.com>
2024-08-17 13:50:17 +0200

    Add non functional LockstepInfoUI

---
+++ Runtime/Scripts/LockstepClientStateEntry.cs
@@ -0,0 +1,37 @@
+﻿using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDKBase;
+using VRC.Udon;
+using TMPro;
+
+namespace JanSharp
+{
+    #if !LockstepDebug
+    [AddComponentMenu("")]
+    #endif
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class LockstepClientStateEntry : UdonSharpBehaviour
+    {
+        #if !LockstepDebug
+        [HideInInspector]
+        #endif
+        [SerializeField] private TextMeshProUGUI clientDisplayNameText;
+        #if !LockstepDebug
+        [HideInInspector]
+        #endif
+        [SerializeField] private TextMeshProUGUI clientStateText;
+        #if !LockstepDebug
+        [HideInInspector]
+        #endif
+        [SerializeField] private GameObject masterPreferenceObject;
+        #if !LockstepDebug
+        [HideInInspector]
+        #endif
+        [SerializeField] private TextMeshProUGUI masterPreferenceText;
+        #if !LockstepDebug
+        [HideInInspector]
+        #endif
+        [SerializeField] private Slider masterPreferenceSlider;
+    }
+}
---
+++ Runtime/Scripts/LockstepInfoUI.cs
@@ -0,0 +1,73 @@
+﻿using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDKBase;
+using VRC.Udon;
+using TMPro;
+
+namespace JanSharp.Internal
+{
+    #if !LockstepDebug
+    [AddComponentMenu("")]
+    #endif
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class LockstepInfoUI : UdonSharpBehaviour
+    {
+        #if !LockstepDebug
+        [HideInInspector]
+        #endif
+        [SerializeField] private GameObject notificationEntryPrefab;
+        #if !LockstepDebug
+        [HideInInspector]
+        #endif
+        [SerializeField] private GameObject clientStateEntryPrefab;
+        #if !LockstepDebug
+        [HideInInspector]
+        #endif
+        [SerializeField] private TextMeshProUGUI localClientStateText;
+        #if !LockstepDebug
+        [HideInInspector]
+        #endif
+        [SerializeField] private TextMeshProUGUI clientCountText;
+        #if !LockstepDebug
+        [HideInInspector]
+        #endif
+        [SerializeField] private GameObject localMasterPreferenceObject;
+        #if !LockstepDebug
+        [HideInInspector]
+        #endif
+        [SerializeField] private TextMeshProUGUI localMasterPreferenceText;
+        #if !LockstepDebug
+        [HideInInspector]
+        #endif
+        [SerializeField] private Slider localMasterPreferenceSlider;
+        #if !LockstepDebug
+        [HideInInspector]
+        #endif
+        [SerializeField] private Toggle notificationLogTabToggle;
+        #if !LockstepDebug
+        [HideInInspector]
+        #endif
+        [SerializeField] private CanvasGroup notificationLogCanvasGroup;
+        #if !LockstepDebug
+        [HideInInspector]
+        #endif
+        [SerializeField] private Transform notificationLogContent;
+        #if !LockstepDebug
+        [HideInInspector]
+        #endif
+        [SerializeField] private ScrollRect notificationLogScrollRect;
+        #if !LockstepDebug
+        [HideInInspector]
+        #endif
+        [SerializeField] private Toggle clientStatesTabToggle;
+        #if !LockstepDebug
+        [HideInInspector]
+        #endif
+        [SerializeField] private CanvasGroup clientStatesCanvasGroup;
+        #if !LockstepDebug
+        [HideInInspector]
+        #endif
+        [SerializeField] private Transform clientStatesContent;
+    }
+}
